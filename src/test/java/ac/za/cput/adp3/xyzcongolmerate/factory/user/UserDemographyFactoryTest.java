93d3746c3ce3d6968f147340a1025a62b7086e8e
Moipone <kthinasonke@gmail.com>
2019-09-03 19:54:57 +0200

    factory implementation testing1

--- src/test/java/ac/za/cput/adp3/xyzcongolmerate/factory/user/UserDemographyFactoryTest.java
+++ src/test/java/ac/za/cput/adp3/xyzcongolmerate/factory/user/UserDemographyFactoryTest.java
@@ -1,26 +1,21 @@
 package ac.za.cput.adp3.xyzcongolmerate.factory.user;
 
-import ac.za.cput.adp3.xyzcongolmerate.domain.user.User;
 import ac.za.cput.adp3.xyzcongolmerate.domain.user.UserDemography;
 import org.junit.Assert;
 import org.junit.Test;
-import sun.util.calendar.BaseCalendar;
 
-import java.time.Instant;
 import java.util.Date;
-
-import static org.junit.Assert.*;
 
 public class UserDemographyFactoryTest {
 
     @Test
     public void buildUserDemography() {
 
-        BaseCalendar.Date bdate = null;
         Date dateobj = new Date();
-        System.out.println(bdate.setNormalizedDate(1998, 09, 18).format(dateobj));
 
         UserDemography userDemography = UserDemographyFactory.buildUserDemography("TinaKabai@absa.co.za",
-                "Ms", "Female", "B2019B", '18:9:1998');
+                "Ms", "Female", "B2019B", dateobj);
+
+        Assert.assertSame(userDemography.getGenderId(), userDemography.getGenderId());
     }
 }
