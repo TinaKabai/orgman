22bbddb95a5f9e318bdf95ce8bf46820a6b5d6d8
Moipone <kthinasonke@gmail.com>
2019-09-03 20:38:33 +0200

    factory implementation testing1

--- src/main/java/ac/za/cput/adp3/xyzcongolmerate/util/Helper.java
+++ src/main/java/ac/za/cput/adp3/xyzcongolmerate/util/Helper.java
@@ -5,15 +5,19 @@ import java.util.UUID;
 public class Helper {
 
     public static String generateRandomGivenSuffix(String suffix) {
+
         return suffix + "-" + UUID.randomUUID().toString();
     }
 
     public static String getClassName(Class<?>  aClass) {
+
         return aClass.getSimpleName();
     }
 
     public static String getSuffixFromClassName(Class<?> aClass) {
         String className = getClassName(aClass);
-        return className;
+
+        return generateRandomGivenSuffix(className);
     }
+
 }
