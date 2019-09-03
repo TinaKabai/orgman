65cc29ca53d937023d490f6590188d0beb04d2d7
Moipone <kthinasonke@gmail.com>
2019-09-03 19:33:38 +0200

    factory implementation testing

--- src/main/java/ac/za/cput/adp3/xyzcongolmerate/factory/user/UserDemographyFactory.java
+++ src/main/java/ac/za/cput/adp3/xyzcongolmerate/factory/user/UserDemographyFactory.java
@@ -6,7 +6,6 @@ import java.util.Date;
 
 public class UserDemographyFactory {
 
-
     public static UserDemography buildUserDemography(String userEmail, String userTitle, String genderId,
                                                      String raceId, Date dateOfBirth) {
 
--- src/test/java/ac/za/cput/adp3/xyzcongolmerate/factory/user/UserDemographyFactoryTest.java
+++ src/test/java/ac/za/cput/adp3/xyzcongolmerate/factory/user/UserDemographyFactoryTest.java
@@ -1,23 +1,26 @@
 package ac.za.cput.adp3.xyzcongolmerate.factory.user;
 
+import ac.za.cput.adp3.xyzcongolmerate.domain.user.User;
+import ac.za.cput.adp3.xyzcongolmerate.domain.user.UserDemography;
+import org.junit.Assert;
 import org.junit.Test;
+import sun.util.calendar.BaseCalendar;
+
+import java.time.Instant;
+import java.util.Date;
 
 import static org.junit.Assert.*;
 
 public class UserDemographyFactoryTest {
 
-    //TODO: implement method body ONLY!
     @Test
     public void buildUserDemography() {
-        throw new UnsupportedOperationException("Not supported yet.");
-        /**
-         * Your implementation goes here
-         *
-         * INSTRUCTION
-         * 1. Remove line [//TODO: implement method body ONLY!]
-         * 2. Remove line [throw new UnsupportedOperationException("Not yet supported.");]
-         * 3. Test the UserDemographyFactory class
-         * 4. Assert that the an object is created.
-         */
+
+        BaseCalendar.Date bdate = null;
+        Date dateobj = new Date();
+        System.out.println(bdate.setNormalizedDate(1998, 09, 18).format(dateobj));
+
+        UserDemography userDemography = UserDemographyFactory.buildUserDemography("TinaKabai@absa.co.za",
+                "Ms", "Female", "B2019B", '18:9:1998');
     }
 }
